40c1ca03ad1c253a38ced5011c46895ce8530e86
Emnajemal <100838789+Emnajemal@users.noreply.github.com>
2023-02-15 19:05:47 +0100

    up

--- gestionsalledesport/src/Form/Cours1Type.php
+++ gestionsalledesport/src/Form/Cours1Type.php
@@ -6,6 +6,7 @@ use App\Entity\Coach;
 use App\Entity\Cours;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 
