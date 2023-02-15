a6d68f223c85261439fd4599cea5605b2ea2d274
Emnajemal <100838789+Emnajemal@users.noreply.github.com>
2023-02-15 18:19:44 +0100

    modif

--- gestionsalledesport/src/Controller/BlogController.php
+++ gestionsalledesport/src/Controller/BlogController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\BlogRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    
+    #[Route('/listblog', name: 'listblog')]
+    public function index(BlogRepository $blogRepository): Response
     {
+        $blog=$blogRepository->findAll();
         return $this->render('blog/blog.html.twig', [
-            'controller_name' => 'BlogController',
+            'blog' => $blog,
+            
         ]);
     }
 }
