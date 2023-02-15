22add54f217d4ff8d3724d8210303c45e4fd4ee2
Emnajemal <100838789+Emnajemal@users.noreply.github.com>
2023-02-15 17:00:40 +0100

    blog-fini

--- gestionsalledesport/migrations/Version20230215133604.php
+++ gestionsalledesport/migrations/Version20230215133604.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230214010059 extends AbstractMigration
+final class Version20230215133604 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20230214010059 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE cours ADD coach_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE cours ADD CONSTRAINT FK_FDCA8C9C3C105691 FOREIGN KEY (coach_id) REFERENCES coach (id)');
-        $this->addSql('CREATE INDEX IDX_FDCA8C9C3C105691 ON cours (coach_id)');
+        $this->addSql('CREATE TABLE blog (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE cours DROP FOREIGN KEY FK_FDCA8C9C3C105691');
-        $this->addSql('DROP INDEX IDX_FDCA8C9C3C105691 ON cours');
-        $this->addSql('ALTER TABLE cours DROP coach_id');
+        $this->addSql('DROP TABLE blog');
     }
 }
