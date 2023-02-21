dd33c4707c83bb15ce09972d96bafcc11eb9a4e5
Emnajemal <100838789+Emnajemal@users.noreply.github.com>
2023-02-21 18:49:10 +0100

    modif

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use App\Kernel;
@@ -7,3 +8,4 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
