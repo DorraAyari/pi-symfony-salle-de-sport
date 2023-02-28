40dd11ec19e897a5d0e7bbf95d533f30e2ffdfe4
najwaD <najwa.dahmeni@esprit.tn>
2023-02-28 19:50:39 +0100

    modf

--- gestionsalledesport/migrations/Version20230216122526.php
+++ gestionsalledesport/migrations/Version20230216122526.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230216105321 extends AbstractMigration
+final class Version20230216122526 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -27,7 +27,7 @@ final class Version20230216105321 extends AbstractMigration
         $this->addSql('CREATE TABLE cours (id INT AUTO_INCREMENT NOT NULL, coach_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, image VARCHAR(255) DEFAULT NULL, description VARCHAR(255) NOT NULL, INDEX IDX_FDCA8C9C3C105691 (coach_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE image (id INT AUTO_INCREMENT NOT NULL, coach_id INT DEFAULT NULL, INDEX IDX_C53D045F3C105691 (coach_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE member (id INT AUTO_INCREMENT NOT NULL, surname VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, age INT NOT NULL, email VARCHAR(255) NOT NULL, telnumber INT NOT NULL, pack VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, `desc` VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, prix VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE produit (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, prix INT NOT NULL, image VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE cours ADD CONSTRAINT FK_FDCA8C9C3C105691 FOREIGN KEY (coach_id) REFERENCES coach (id)');
         $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045F3C105691 FOREIGN KEY (coach_id) REFERENCES coach (id)');
--- gestionsalledesport/src/Entity/Produit.php
+++ gestionsalledesport/src/Entity/Produit.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\ProduitRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProduitRepository::class)]
@@ -18,16 +21,33 @@ class Produit
 
  
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"NOM est obligatoir")]
+    
     private ?string $nom = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Description est obligatoir")]
+    #[Assert\Length( min: 2,max: 200,)]
     private ?string $description = null;
 
     #[ORM\Column]
-    private ?int $prix = null;
+    
+    #[Assert\NotBlank(message:"Prix est obligatoir")]
+    #[Assert\Positive]
+    private ?int $prix;
 
     #[ORM\Column(length: 255)]
+    
+    
     private ?string $image = null;
+
+    #[ORM\OneToMany(mappedBy: 'Cmd', targetEntity: Command::class)]
+    private Collection $cmd;
+
+    public function __construct()
+    {
+        $this->Cmd = new ArrayCollection();
+    }
 
 
     public function getId(): ?int
@@ -104,6 +124,36 @@ class Produit
     public function setImage(string $image): self
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Command>
+     */
+    public function getCmd(): Collection
+    {
+        return $this->Cmd;
+    }
+
+    public function addCmd(Command $Cmd): self
+    {
+        if (!$this->Cmd->contains($Cmd)) {
+            $this->Cmd->add($Cmd);
+            $Cmd->setCmd($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCmd(Command $Cmd): self
+    {
+        if ($this->Cmd->removeElement($Cmd)) {
+            // set the owning side to null (unless already changed)
+            if ($Cmd->getCmd() === $this) {
+                $Cmd->setCmd(null);
+            }
+        }
 
         return $this;
     }
